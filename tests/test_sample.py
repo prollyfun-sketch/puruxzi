77c77276d28d47a739b0d6239dcf41a2ac706161
Your Name <you@example.com>
2025-12-02 13:37:33 -0500

    Add subtraction, multiplication, division tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,2 +1,8 @@
-def test_addition():
-    assert 2 + 2 == 4
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 4 * 2 == 8
+
+def test_division():
+    assert 8 / 2 == 4
